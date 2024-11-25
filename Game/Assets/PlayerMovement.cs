f2908cb58f8d517571853f51e75cdbb98c4d0a8e
Hlib Osmakov <to.glebo@gmail.com>
2024-11-25 09:08:57 +0200

    demo scene

---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovementTopDown : MonoBehaviour
+{
+    public float speed = 5f; // Швидкість руху
+    public FixedJoystick joystick; // Посилання на джойстик
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Отримання напрямку руху від джойстика
+        Vector2 direction = new Vector2(joystick.Horizontal, joystick.Vertical);
+
+        // Переміщення гравця
+        rb.velocity = direction * speed;
+    }
+}
